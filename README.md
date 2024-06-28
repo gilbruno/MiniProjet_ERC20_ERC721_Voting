d15de4700356d0cace85a06dbccd538902cde2fd
gilbruno <gilles_bruno@gmx.fr>
2024-06-28 15:14:22 +0200

    new features for SC voting

--- README.md
+++ README.md
@@ -18,6 +18,9 @@ cast send 0x2aF3c4E3a6ee455294693B977a2D3DABf419B3B5 "mint(address,uint256)" 0xB
 
 if any issue with these commands, you can add : _--legacy_ options
 
+
+Address USDV deployed on Sepolia : 0x2aF3c4E3a6ee455294693B977a2D3DABf419B3B5
+
 ## Deploy NFT Collection on Sepolia
 
 ```shell
@@ -32,7 +35,7 @@ if any issue with these commands, you can add : _--legacy_ options
 cast send <NFT_Smartcontract_address> "mint(address,string)" 0xB63cE207B985e8508b17B5e8CF3900EF22C4e444 "https://ipfs.io/ipfs/QmNf1UsmdGaMbpatQ6toXSkzDpizaGmC9zfunCyoz1enD5/penguin/1337.png" --rpc-url $SEPOLIA_RPC_URL --private-key $SEPOLIA_PRIVATE_KEY
 ```
 
-Address SC deployed : 0xa727E67fFf460be1D40E638481C034F1836140F6
+Address NFT SC deployed : 0xa727E67fFf460be1D40E638481C034F1836140F6
 
 ## TP
 
--- src/SimpleVotingSystem.sol
+++ src/SimpleVotingSystem.sol
@@ -1,38 +1,71 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
+import {AccessControl} from "openzeppelin-contracts/contracts/access/AccessControl.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {IERC721} from "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
 
-contract SimpleVotingSystem {
+enum  WorkflowStatus {
+    INIT,
+    REGISTER_CANDIDATES,
+    FUND_SESSION,
+    VOTE_SESSION,
+    VOTE_COUNT
+}
+
+error BadStatusRegisterCandidatesError();
+
+error BadStatusFundSession();
+
+error BadStatusVoteSession();
+
+error NotEnoughAmountForFunding();
+
+contract SimpleVotingSystem is Ownable, AccessControl {
+
+    IERC20 private cryptoAccepted;
+    IERC721 private nftAccepted;
+
     struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
 
-    address public owner;
     mapping(uint => Candidate) public candidates;
     mapping(address => bool) public voters;
     uint[] private candidateIds;
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the contract owner can perform this action");
-        _;
+    WorkflowStatus private workflowStatus;
+
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant VOTER_ROLE = keccak256("VOTER_ROLE");
+    bytes32 public constant FUNDER_ROLE = keccak256("FUNDER_ROLE");
+    
+    uint public MIN_AMOUNT_FOR_FUNDING = 100;
+
+    constructor(address addressCryptoAccepted, address addressNftAccepted) Ownable(msg.sender) {
+        cryptoAccepted = IERC20(addressCryptoAccepted);
+        nftAccepted    = IERC721(addressNftAccepted);
+        _grantRole(ADMIN_ROLE, msg.sender);       
     }
 
-    constructor() {
-        owner = msg.sender;
-    }
-
-    function addCandidate(string memory _name) public onlyOwner {
+    function setWorkflowStatus(WorkflowStatus _num) public onlyRole(ADMIN_ROLE) {
+        workflowStatus = WorkflowStatus(_num);
+       } 
+     
+    function addCandidate(string memory _name) public onlyRole(ADMIN_ROLE) {
         require(bytes(_name).length > 0, "Candidate name cannot be empty");
+        if (workflowStatus != WorkflowStatus.REGISTER_CANDIDATES) revert BadStatusRegisterCandidatesError();
         uint candidateId = candidateIds.length + 1;
         candidates[candidateId] = Candidate(candidateId, _name, 0);
         candidateIds.push(candidateId);
     }
 
-    function vote(uint _candidateId) public {
+    function vote(uint _candidateId) public onlyRole(VOTER_ROLE){
         require(!voters[msg.sender], "You have already voted");
         require(_candidateId > 0 && _candidateId <= candidateIds.length, "Invalid candidate ID");
-
+        if (workflowStatus != WorkflowStatus.VOTE_SESSION) revert BadStatusVoteSession();
         voters[msg.sender] = true;
         candidates[_candidateId].voteCount += 1;
     }
@@ -40,6 +73,24 @@ contract SimpleVotingSystem {
     function getTotalVotes(uint _candidateId) public view returns (uint) {
         require(_candidateId > 0 && _candidateId <= candidateIds.length, "Invalid candidate ID");
         return candidates[_candidateId].voteCount;
+    }
+
+    function fundForCampaign(uint256 amount) public onlyRole(FUNDER_ROLE) {
+        if (workflowStatus != WorkflowStatus.FUND_SESSION) revert BadStatusFundSession();
+        uint balanceOfFunder = cryptoAccepted.balanceOf(msg.sender);
+        if (balanceOfFunder < MIN_AMOUNT_FOR_FUNDING) revert NotEnoughAmountForFunding();
+        cryptoAccepted.transferFrom(msg.sender, address(this), amount);
+    }
+
+    function requestGrantForFunding() public {
+        _grantRole(FUNDER_ROLE, msg.sender);
+    }
+
+    function requestGrantForVoting() public {
+        uint balanceOfNfts = nftAccepted.balanceOf(msg.sender);
+        if (balanceOfNfts >= 1) {
+            _grantRole(VOTER_ROLE, msg.sender);
+        }
     }
 
     function getCandidatesCount() public view returns (uint) {
@@ -50,5 +101,17 @@ contract SimpleVotingSystem {
     function getCandidate(uint _candidateId) public view returns (Candidate memory) {
         require(_candidateId > 0 && _candidateId <= candidateIds.length, "Invalid candidate ID");
         return candidates[_candidateId];
+    }
+
+    function getWorkflowStatus() public view returns (WorkflowStatus) {
+        return workflowStatus;
+    }
+
+    function getCryptoAccepted() public view returns (IERC20) {
+        return cryptoAccepted;
+    }
+
+    function getNftAccepted() public view returns (IERC721) {
+        return nftAccepted;
     }
 }
